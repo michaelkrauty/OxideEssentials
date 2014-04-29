ce184f6fa804d7d4e77894eda6659b27b1fdce8d
Daniel Oxenbury <daniel.oxenbury@hotmail.co.uk>
2014-04-29 14:05:36 +0100

    Update OxideEssentials.lua

--- OxideEssentials.lua
+++ OxideEssentials.lua
@@ -1,6 +1,19 @@
 PLUGIN.Title = "OxideEssentials"
 PLUGIN.Description = "A bundle of useful commands essential for any Rust server unning Oxide."
+PLUGIN.Author = "Daniel & Michael"
+PLUGIN.Version = "1.0"
 
+-- Called when oxide loads or user types oxide.reload example at F1 console
 function PLUGIN:Init()
-	
+    self:AddChatCommand("test", self.cmdList)
+end
+
+-- Called when user types /test
+function PLUGIN:cmdList( netuser, cmd, args )
+    rust.SendChatToUser( netuser, "Your plugin works!" )
+end
+
+-- Automated Oxide help function (added to /help list)
+function PLUGIN:SendHelpText( netuser )
+	rust.SendChatToUser( netuser, "Use /example to spawn in a pillar behind your head." )
 end
